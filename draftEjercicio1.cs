739a5856bb7f3899c358b59d2103be2c61d928e0
juandavidvch <juandavidvch@gmail.com>
2016-10-11 00:16:19 -0600

    subir draft ejercicio1

---
+++ draftEjercicio1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenI
+{
+    public class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            string input = "28 64 54|125 35 12|100 25 6";
+            DiagonalDifference calc = new DiagonalDifference();
+            calc.matrix(input);
+        }
+
+
+        public int matrix(string word ) //metodo de tipo entero que recibe un string
+        {
+            char delimiter = '|';
+           // char delim = ' ';
+            int sum1=0 , sum2=0, difference=0;
+            string [] separated = word.Split(delimiter);
+            for (int i = 0; i < separated.Length; i++ )
+            {
+                Console.WriteLine(separated[i]);
+            }
+
+
+
+
+                //int [,] array;
+                //int o;
+                //var a = word.Split(ng[] {"|"},StringSplitOptions.RemoveEmptyEntries);
+                //foreach (string s in a)
+                //{
+                //    o = Convert.ToInt16(a);
+                //    //for (int i=0, i<a.Length, i++)
+
+                //    {
+
+                //    }
+
+                //}
+
+
+
+                difference = sum1 - sum2;
+            return difference;
+        }
+    }   
+}
